47bf3858ebe90239e2b876b92dfed6403adb94bf
Prasanna <sprasannasrinivasarao@gmail.com>
2025-06-22 21:48:49 +0530

    Update sample.py
    update

--- sample.py
+++ sample.py
@@ -1,28 +1,48 @@
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
-import json
+from awsglue.job import Job
+from awsglue.utils import getResolvedOptions
+
+import sys
 import os
+import json
 
-# Local param file path
-param_file_path = os.path.join("config", "job_params.json")
-
+# -------------------------------
 # Read parameters from local file
+# -------------------------------
+param_file_path = os.path.join("config", "job_params.json")
 with open(param_file_path, "r") as f:
     params = json.load(f)
 
-# Access parameters
-name = params["name"]
-age = params["age"]
+# Example parameters
+name = params.get("name", "Unknown")
+age = params.get("age", 0)
 
-# Initialize Spark and Glue Contexts
+# -------------------------------
+# AWS Glue boilerplate setup
+# -------------------------------
+#args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+args = {'JOB_NAME': 'test_job'}
+
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 
-# Use parameters in logic
-df = spark.createDataFrame([{"name": name, "age": age}])
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# -------------------------------
+# Main ETL Logic
+# -------------------------------
 print("🔥 Hello from Spark in Glue 5 (Local Mode)!")
+
+df = spark.createDataFrame([{"name": name, "age": age}])
 df.show()
 
+# -------------------------------
+# Commit the job (important in Glue)
+# -------------------------------
+job.commit()
 
-print("this is a change test to overwrite docker image!!!!!!!!!!")
+
+
